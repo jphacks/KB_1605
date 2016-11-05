8be33aa1d1339eef4812603e5260b029da264267
藤澤 卓馬 <takuma@fujisawa-no-MacBook-Air.local>
2016-11-05 17:31:21 +0900

    commit

--- public/javascripts/linebot.js
+++ public/javascripts/linebot.js
@@ -4,7 +4,7 @@ var request = require('request');
 var accessToken = 'PYx43JaDryJhsLiuRAb4sfrSbjs+WQl3woWROaYvR0Qi94PLiBacIFBo1/dzJ43MsZJuN2bGQ8ZbthKBKDy1hT2gqLR61qQE8D9mXDuA002PAgpm45VMymQW30mQuDFxxFFAYrhZ2xbyzc8oATVMIgdB04t89/1O/w1cDnyilFU=';
 
 exports.send = function(req){
-	console.log(req);
+	console.log(req.events);
 	var options = {
 		url: 'https://api.line.me/v2/bot/message/push',
 		headers: {
