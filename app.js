abd697fad57b69e2c52f62db28393c5121571be6
藤澤 卓馬 <takuma@fujisawa-no-MacBook-Air.local>
2016-11-06 10:28:13 +0900

    commit

--- app.js
+++ app.js
@@ -50,7 +50,9 @@ app.get('/send', function(req, res){
 app.post('/callback', function(req, res){
 	var event = req.body.events[0];
 
-	mysql.showTables();
+	mysql.showTables().then(function(rows){
+		console.log(rows);
+	});
 	mysql.dropTable('test');
 	mysql.createTable('test');
 
