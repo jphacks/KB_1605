73338226b7717e47c257876b5a2c20ed341c6a67
藤澤 卓馬 <takuma@fujisawa-no-MacBook-Air.local>
2016-11-06 10:01:20 +0900

    commit

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.get('/send', function(req, res){
 app.post('/callback', function(req, res){
 	var event = req.body.events[0];
 
-	if(event.type === 'follow') {
+	if(event.type === 'follow')
 		mysql.insert('test', 'name', event.source.userId);
 	else if(event.type === 'message') linebot.reply(event.replyToken, 'message received');
 
